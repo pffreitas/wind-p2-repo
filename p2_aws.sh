71c5281b85893010dd3b5c25acfd8ac6715276f3
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2012-04-11 01:03:43 +0000

    git-svn-id: http://wind-platform.googlecode.com/svn/trunk/wind-platform/rap-target@816 7bc07f76-e8d3-11dd-b2d9-215335d0b316

--- p2_aws.sh
+++ p2_aws.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-/home/ubuntu/eclipse/eclipse  -application org.eclipse.equinox.p2.publisher.EclipseGenerator \
-   -source /var/lib/hudson/jobs/Wind Target Runtime/workspace/rap-target/eclipse \
-   -metadataRepository file:/var/lib/hudson/jobs/Wind Target Runtime/workspace/rap-target/p2/repo \
+java -jar /home/ubuntu/eclipse/plugins/org.eclipse.equinox.launcher_*.jar -console -consolelog -application org.eclipse.equinox.p2.publisher.EclipseGenerator \
+   -source "/var/lib/hudson/jobs/Wind Target Runtime/workspace/rap-target/eclipse" \
+   -metadataRepository "file:/var/lib/hudson/jobs/Wind Target Runtime/workspace/rap-target/p2/repo" \
    -metadataRepositoryName "Wind Target Runtime" \
-   -artifactRepository file:/var/lib/hudson/jobs/Wind Target Runtime/workspace/rap-target/p2/repo \
+   -artifactRepository "file:/var/lib/hudson/jobs/Wind Target Runtime/workspace/rap-target/p2/repo" \
    -artifactRepositoryName "Wind Target Runtime" \
    -publishArtifacts \
    -publishArtifactRepository \
